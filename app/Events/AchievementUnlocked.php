5634632805473d78cd79e37bd36a92904dca29b0
Francis Wanyeki <Wanyekifs@gmail.com>
2024-01-12 16:01:04 +0300

    add tests for unlocking an achievement on comment

---
+++ app/Events/AchievementUnlocked.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Achievement;
+use App\Models\User;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AchievementUnlocked
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Achievement $achievement, User $user)
+    {
+
+        $this->achievement = $achievement;
+        $this->user = $user;
+        $this->payload = [
+            "achievement_name" => $achievement->name,
+            "user" => $user,
+        ];
+    }
+}
---
+++ tests/Feature/AchievementUnlockedOnCommentTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Events\AchievementUnlocked;
+use App\Events\CommentWritten;
+use App\Listeners\CheckAndUnlockAchievement;
+use App\Models\Achievement;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class AchievementUnlockedOnCommentTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_correct_achievement_is_unlocked_after_writing_a_comment(): void
+    {
+        $this->artisan('db:seed');
+        $achievements = Achievement::where("type", "comment")->get();
+
+        foreach ($achievements as $achievement) {
+            $user = $this->handleCommentWrittenEvent($achievement->threshold);
+            Event::assertDispatched(AchievementUnlocked::class, function ($event) use ($achievement, $user) {
+                return $event->payload['achievement_name'] == $achievement->name && $event->payload['user']->id == $user->id;
+            }, );
+        }
+    }
+
+    private function handleCommentWrittenEvent(int $numberOfComments): User
+    {
+        Event::fake();
+        $user = User::factory()->create();
+        $comments = Comment::factory()->count($numberOfComments)->create(["user_id" => $user->id]);
+        $lastComment = $comments->last();
+        $event = new CommentWritten($lastComment);
+        $listener = new CheckAndUnlockAchievement();
+        $listener->handle($event);
+
+        return $user;
+    }
+}
