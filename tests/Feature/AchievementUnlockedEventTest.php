50ab2f98c8597394fb836707c37301767e9a12d8
Francis Wanyeki <Wanyekifs@gmail.com>
2024-01-12 17:51:09 +0300

    fix achievement Unlock event test

--- tests/Feature/AchievementUnlockedEventTest.php
+++ tests/Feature/AchievementUnlockedEventTest.php
@@ -2,19 +2,30 @@
 
 namespace Tests\Feature;
 
+use App\Events\AchievementUnlocked;
+use App\Listeners\CheckAndUnlockBadge;
+use App\Models\Achievement;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class AchievementUnlockedEventTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    use RefreshDatabase;
+    public function test_unlock_achievement_listener_handles_comment_event(): void
     {
-        $response = $this->get('/');
+        $this->initAndFireEvent();
+        Event::assertListening(AchievementUnlocked::class, CheckAndUnlockBadge::class);
+    }
+    private function initAndFireEvent(): void
+    {
+        Event::fake();
+        $user = User::factory()->create();
+        $achievement = Achievement::limit(1)->get()->first();
 
-        $response->assertStatus(200);
+        $user->achievements()->attach($achievement);
+        event(new AchievementUnlocked($achievement, $user));
     }
 }
